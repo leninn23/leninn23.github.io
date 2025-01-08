5773a143c65b18c52d49ed6adb1288526a158544
leninn23 <leninanderson23@gmail.com>
2025-01-08 17:45:13 +0100

    updatejs

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Evita el envío del formulario
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    // Validación básica
-    if (name && email && message) {
-        document.getElementById('formMessage').textContent = "¡Gracias por tu mensaje! Te responderé pronto.";
-        document.getElementById('formMessage').style.color = "green";
-
-        // Limpia el formulario
-        this.reset();
-    } else {
-        document.getElementById('formMessage').textContent = "Por favor, completa todos los campos.";
-        document.getElementById('formMessage').style.color = "red";
-    }
-});
